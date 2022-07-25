27da12c74f189dd1deb688939cb649190d9cd7ad
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-07-25 23:55:10 +0430

    Update deploy.py

--- contracts/scripts/deploy.py
+++ contracts/scripts/deploy.py
@@ -6,4 +6,3 @@ def main():
     contract = nftMinter.deploy({"from": account}, publish_source=True)
     print(f"Your contract deployed at {contract}")
 
-    #0x5a34EAD70355f8652DCB5bB808036ef221F5F451
