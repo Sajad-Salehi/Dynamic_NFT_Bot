ac821fa92acfa38232117a0e5eae6f2cfd26fb04
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-07-29 23:37:49 +0430

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 import os
 import telebot
+from metadata import upload_file, upload_metadata
 from bottons import keyboard1, keyboard2, keyboard3
-from private_key import set_private_key, get_user_info
+from userInfo import set_private_key, get_user_info, set_metadata
 from responses import myWallet, myAccount, addAccount, getPrivateKey, static, dynamic, mint
 
 
+metadata = {
+    "name": None,
+    "description": None,
+    "image": None,
+    "attributes": [{}]
+}
 api_key = os.getenv('API_KEY')
 bot = telebot.TeleBot(api_key)
 
@@ -57,13 +64,51 @@ def setPrivateKey(message):
 @bot.message_handler(func=static)
 def MintStaticNFT(message):
 
-    bot.send_message(message.chat.id, 'saaalm static')
+    text = 'Alright. Let`s set up your NFT metadata.\nPlease send NFT image: '
+    bot.send_message(message.chat.id, text)
 
 
 @bot.message_handler(func=dynamic)
 def MintDynamicNFT(message):
 
     bot.send_message(message.chat.id, 'saaalam dynamic')
+
+
+@bot.message_handler(content_types=['photo'])
+def get_user_pics(message):
+    
+    username = message.from_user.username
+    id = message.photo[-1].file_id
+    info = bot.get_file(id)
+    x = bot.download_file(info.file_path)
+
+    with open('image.jpg', 'wb') as new_file:
+        new_file.write(x)
+
+    image_url = upload_file()
+
+    set_metadata(image_url, username)
+    send = bot.send_message(message.from_user.id, "Send NFT title: ")
+    bot.register_next_step_handler(send, get_name)
+
+
+def get_name(message):
+
+    title = message.text
+    username = message.from_user.username
+    set_metadata(title, username)
+    send = bot.send_message(message.from_user.id, "Send NFT description: ")
+    bot.register_next_step_handler(send, get_desc)
+
+def get_desc(message):
+
+    desc = message.text
+    username = message.from_user.username
+    set_metadata(desc, username)
+    metadata_url = upload_metadata(username)
+    print(metadata_url)
+    bot.send_message(message.from_user.id, f"Nice! Your minted successfully.\nSee your NFT in OpenSea: {''}")
+    
 
 
 bot.polling()
