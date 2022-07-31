5a21edde91077485ef6aadf29d0ca5cba70aa3e7
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-07-31 20:31:29 +0430

    Update deploy_dynamic.py

--- contracts/scripts/deploy_dynamic.py
+++ contracts/scripts/deploy_dynamic.py
@@ -6,18 +6,3 @@ def main():
     contract = nftMinter.deploy({"from": account})
     print(f"Your contract deployed at {contract}")
 
-'''
-    
-    uri_1 = 'https://ipfs.io/ipfs/QmeFfKSzbfT9YJutBPFNU3pTRPfSHqfReP2xyZUCtvmTsY'
-    uri_2 = 'https://ipfs.io/ipfs/QmVkfzS11dJ5WRV4tQi3LC9Gvx4QUzULydGXjSjKhBBbNr'
-    uri_3 = 'https://ipfs.io/ipfs/QmUpdLbS4HpedgDkQFemB49eMCeppkZJ43mUFWv7ysnki5'
-
-    trx = contract.set_uri(uri_1, uri_2, uri_3, {'from': account})
-    trx.wait(2)
-    print(trx)
-
-    trx = contract.mint_nft({'from': account})
-    trx.wait(2)
-    print(trx)
-    
-'''
