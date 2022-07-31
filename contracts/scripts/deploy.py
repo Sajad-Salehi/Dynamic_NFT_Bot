93646fe1db104da298adfbd6a7f3985b3288d459
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-07-31 10:55:58 +0430

    Delete deploy.py

--- contracts/scripts/deploy.py
+++
@@ -1,8 +0,0 @@
-from brownie import accounts, nftMinter
-
-def main():
-
-    account = accounts.load('dev1')
-    contract = nftMinter.deploy({"from": account}, publish_source=True)
-    print(f"Your contract deployed at {contract}")
-
